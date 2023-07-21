4724e76506db8696a55a0b1fa0ba2c796b4d54b9
Xau <frozzy.xfs@gmail.com>
2023-07-20 23:05:16 -0400

    Add basic tic-tac-toe display

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+var debug = 0;
+
+const gameController = (() => {
+})();
+
 const gameBoard = (() => {
     let board = ["", "", "",
                  "", "", "",
                  "", "", ""];
 
     const getBoardState = () => board;
+    const updateDisplay = () => {
+        cells = document.querySelectorAll(`.cell`);
+        cells.forEach(cell => {
+            cell.textContent = board[cell.getAttribute('data-id')];
+        });
+    }
     const clear = () => {
         board.fill("");
+        updateDisplay();
     };
     const isValid = (x, y) => board[x + y * 3] === "";
     const play = (player, x, y) => {
@@ -19,12 +31,20 @@ const gameBoard = (() => {
         if (isValid(x, y)){
             let index = x + y * 3;
             board[index] = player.symbol;
+
+            updateDisplay();
+
+            return 1;
         }
+        else return 0;
     };      
-    return {getBoardState, clear, play};
+    if (debug) board.fill("D");
+    console.log(debug);
+    return {getBoardState, updateDisplay, clear, play};
 })();
 
 console.log(gameBoard.getBoardState());
 
 gameBoard.play({symbol: "X"}, 1, 1);
 gameBoard.play({symbol: "X"}, 1, 1);
+gameBoard.updateDisplay();
